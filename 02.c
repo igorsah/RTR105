32782ed34be93ec58cf1ea117b5fec5be6852f99
igorsah <55091352+igorsah@users.noreply.github.com>
2019-09-23 17:54:55 +0300

    Add files via upload

---
+++ 02.c
@@ -0,0 +1,11 @@
+void main()
+ {
+ printf("hello world\n");
+ printf("hello world\r");
+ printf("hello world\t");
+ printf("hello world\a");
+ printf("hello world\'");
+ printf("hello world\"");
+ printf("hello world\?");
+ printf("hello world\\");
+ }
---
+++ 03.c
@@ -0,0 +1,4 @@
+void main()
+ {
+ printf("Hello Universe? \n");
+ }
