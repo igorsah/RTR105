f0da381fd095571ac1e408083786df587c02bee4
igorsah <55091352+igorsah@users.noreply.github.com>
2019-09-30 16:09:56 +0300

    Add files via upload

--- 02.c
+++ 02.c
@@ -1,11 +1,13 @@
-void main()
- {
- printf("hello world\n");
- printf("hello world\r");
- printf("hello world\t");
- printf("hello world\a");
- printf("hello world\'");
- printf("hello world\"");
- printf("hello world\?");
- printf("hello world\\");
- }
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	int a = 50000;
+	int b = 1000000;
+	int c = a * b
+	printf ("int datu tipa izmers ri: #d baiti \n"), sizeof (int) );
+	printf (" Apreekijinam a un b reizinaajumu :\n" );
+	printf ("a = %d, d = %d \n", a, b;
+	printf ("c = a * b = %d * %d = %d /n", a,b,c );
+}
