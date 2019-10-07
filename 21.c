1288d46bc8cd59b8f5eb46d0b1293984808fae76
igorsah <55091352+igorsah@users.noreply.github.com>
2019-10-07 16:00:19 +0300

    Add files via upload

---
+++ 21.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+main ()
+{
+int a=100;
+printf("operacijas >>: %d>>1 rezultats %d\n",a,a>>1);
+int b;
+printf("b mainiga \sakuma`stavoklis (uzreiz pec deklaresanas): %d\n",b);
+b=a>>2;
+printf("operacija >>: %d>>1%d rezultats %d\n",a,2,b);
+int c;
+printf("Cienijamais lietotaj, ludzu, ievadi operacijas 1. operatoru:");
+scanf:("/d",&a);
+printf("Cienijamais lietotaj, ludzu, ievadi operacijas 2. operatoru:");
+scanf:("/d",%a);
+}
